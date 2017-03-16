07a9cd13f9dad22fcc3a52fa7d2efb348d330f95
dantegg <luroufan327@hotmail.com>
2017-03-17 00:02:03 +0800

    update

--- src/app.vue
+++ src/app.vue
@@ -6,8 +6,8 @@
             <li><router-link to='/foo'>foo</router-link></li>
             <li><router-link to='/bar'>bar</router-link></li>
         </ul>
-        <el-button>默认按钮</el-button>
-        <el-button type="primary">主要按钮</el-button>
+        <el-button v-on:click='dButton'>默认按钮</el-button>
+        <el-button type="primary" v-on:click='pButton'>主要按钮</el-button>
         <el-button type="text">文字按钮</el-button>
         <router-view></router-view>
     </div>
@@ -19,6 +19,20 @@
         data(){
             return {
                 msg:'hello vue2!!!'
+            }
+        },
+        methods:{
+            dButton:function(){
+                console.log('foo',this)
+                this.$router.push({
+                    path:'foo'
+                })
+            },
+            pButton:function(){
+                console.log('bar')
+                this.$router.push({
+                    path:'bar'
+                })
             }
         }
     }
--- src/first.vue
+++ src/first.vue
@@ -1,9 +1,33 @@
 <template>
-    <div>first vue component</div>
+    <div>
+        <div class="first-cp">first vue component</div>
+        <div>
+            <el-input v-model="input" placeholder="请输入内容"></el-input>
+            <el-input v-model='input2' placeholder='请输入内容'></el-input>
+        </div>
+    </div>
+    
 </template>
 
 <<script>
     export default{
-        name:'first'
+        data(){
+            return{
+                input:"",
+                input2:'123'
+            }
+        }
     }
 </script>
+
+<style>
+    .first-cp{
+        color:green;
+        font-size:25px;
+    }
+
+    .first-cp:hover{
+        color:red;
+        font-size:35px;
+    }
+</style>
